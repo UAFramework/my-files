2340c94e55ef8e61266fe759ef7f29a4fb1e2b2c
iamoksanao200 <oksanao200@gmail.com>
2024-07-24 12:04:23 +0300

    bag fixed

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,8 +1,18 @@
 import './ItemCard.css'
 import { Folder as FolderIcon } from 'grommet-icons'
+<<<<<<< HEAD
 import FileIcon from './FileIcon'
 
 const ItemCard = ({ item, fetchFiles }) => {
+=======
+import { Layer, Box } from 'grommet'
+import { useState } from 'react'
+import FileIcon from './FileIcon'
+
+const ItemCard = ({ item, fetchFiles }) => {
+  const [showFileContent, setShowFileContent] = useState(false)
+
+>>>>>>> tmp
   const splittedNameArray = item.name.split('.')
   const fileExtension = splittedNameArray[splittedNameArray.length - 1]
 
@@ -11,6 +21,23 @@ const ItemCard = ({ item, fetchFiles }) => {
   const handleFileClick = () => {
     // TODO:
     // implement this handler
+<<<<<<< HEAD
+=======
+    setShowFileContent(true)
+  }
+
+  const onClose = () => setShowFileContent(false)
+
+  const styleIframe = (e) => {
+    const style = document.createElement('style')
+    style.textContent = `
+      body { display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }`
+    e.target.contentDocument.head.appendChild(style)
+>>>>>>> tmp
   }
 
   return (
@@ -27,6 +54,21 @@ const ItemCard = ({ item, fetchFiles }) => {
             {item.name}
             <p className='file-subtitle'>{item.size}</p>
           </div>
+<<<<<<< HEAD
+=======
+          {showFileContent && (
+            <Layer position='center' onClickOutside={onClose} onEsc={onClose}>
+              <Box pad='medium' gap='small' width='large' height='large'>
+                <iframe
+                  style={{ width: '100%', height: '100%', border: 'none' }}
+                  src={`/api/content?path=${item.path}`}
+                  title={item.name}
+                  onLoad={styleIframe}
+                />
+              </Box>
+            </Layer>
+          )}
+>>>>>>> tmp
         </div>
       )}
     </li>
