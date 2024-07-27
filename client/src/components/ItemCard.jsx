10ea76d695418d1f39c0957a143d84cb09d1305e
Pavlo <pavel.filkovskiy@gmail.com>
2024-07-27 10:44:19 +0100

    UAF-7: implement Layer (#5)

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,16 +1,33 @@
 import './ItemCard.css'
 import { Folder as FolderIcon } from 'grommet-icons'
+import { Layer, Box } from 'grommet'
+import { useState } from 'react'
 import FileIcon from './FileIcon'
 
 const ItemCard = ({ item, fetchFiles }) => {
+  const [showFileContent, setShowFileContent] = useState(false)
+
   const splittedNameArray = item.name.split('.')
   const fileExtension = splittedNameArray[splittedNameArray.length - 1]
 
   const handleFolderClick = () => fetchFiles(item.path)
 
   const handleFileClick = () => {
-    // TODO:
-    // implement this handler
+    setShowFileContent(true)
+  }
+
+  const onClose = () => setShowFileContent(false)
+
+  const styleIframe = (e) => {
+    const style = document.createElement('style')
+    style.textContent = `
+      body { display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: #000;
+      }`
+    e.target.contentDocument.head.appendChild(style)
   }
 
   return (
@@ -27,6 +44,19 @@ const ItemCard = ({ item, fetchFiles }) => {
             {item.name}
             <p className='file-subtitle'>{item.size}</p>
           </div>
+          {showFileContent && (
+            <Layer position='center' onClickOutside={onClose} onEsc={onClose}>
+              <Box id='item-card-container' pad='medium' gap='small' width='large' height='large'>
+                <Box>{item.name}</Box>
+                <iframe
+                  style={{ width: '100%', height: '100%', border: 'none' }}
+                  src={`/api/content?path=${item.path}`}
+                  title={item.name}
+                  onLoad={styleIframe}
+                />
+              </Box>
+            </Layer>
+          )}
         </div>
       )}
     </li>
